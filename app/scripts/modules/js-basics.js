84b2e4a2e94160f60b74c0aa20729faf2792f254
Lucas BARBAY <lucas.barbay@gmail.com>
2015-10-08 23:37:13 +0200

    add 4 JSBAsics tests

--- app/scripts/modules/js-basics.js
+++ app/scripts/modules/js-basics.js
@@ -5,28 +5,43 @@
     var toto;
 
     JS_BASICS.isNumberEven = function(i) {
-        // i will be an integer.
-        // Return true if it's even, and false if it isn't.
+        return i % 2 === 0;
     };
 
     JS_BASICS.getFileExtension = function(str) {
-        // str will be a string, but it may not have a file extension.
-        // Return the file extension (with no period) if it has one, otherwise false
+        return (str.lastIndexOf(".") === -1) ? false : str.substring(str.lastIndexOf(".") + 1, str.length);
     };
 
     JS_BASICS.longestString = function(arr) {
-        // arr will be an array.
-        // Return the longest string in the array
+        var maxString, i, maxLength = 0;
+        for (i = 0; i < arr.length; i++) {
+            if (arr[i].length > maxLength && typeof arr[i] == "string") {
+                maxString = arr[i];
+                maxLength = arr[i].length;
+            }
+        }
+        return maxString;
     };
 
     JS_BASICS.reverseString = function(str) {
         // str will be an string
         // Return a new string who's characters are in the opposite order to str's.
+        return str.split("").reverse().join("");
     };
 
     JS_BASICS.isPalindrome = function(str) {
         // str will be an string
         // Return true if it is a palindrome and false otherwise. It should be case insensitive and not consider space or punctuation.
+        /*for( var i = str.length; i > 0; i-- ){
+            if(str[i] == str.charAt(str.length) - 1){
+                return true;
+            } else {
+                return false;
+            }
+        }*/
+        var cleaned = str.toLowerCase().match(/[a-z]/gi).reverse();
+        return cleaned.join('') === cleaned.reverse().join('');
+
     };
 
     JS_BASICS.nestedSum = function(arr) {
