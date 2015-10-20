211b07bca2a644a693356058894a7cdf24768d0c
Lucas BARBAY <lucas.barbay@gmail.com>
2015-10-20 10:41:13 +0200

    fix travis issue (problem with isInteger function which returned an undefined function message)

--- app/scripts/modules/js-basics.js
+++ app/scripts/modules/js-basics.js
@@ -24,14 +24,10 @@
     };
 
     JS_BASICS.reverseString = function(str) {
-        // str will be an string
-        // Return a new string who's characters are in the opposite order to str's.
         return str.split("").reverse().join("");
     };
 
     JS_BASICS.isPalindrome = function(str) {
-        // str will be an string
-        // Return true if it is a palindrome and false otherwise. It should be case insensitive and not consider space or punctuation.
         str = str.replace(/ /g,"");
         str = str.toLowerCase();
         var i1 = 0;
@@ -40,23 +36,18 @@
             if (str[i1] != str[i2]) {
                 return false;
             }
-            ++i1;
-            --i2;
+            ++i1; --i2;
         }
         return true;
-
-
     };
 
     JS_BASICS.nestedSum = function(arr) {
-        // arr will be an array, containing integers, strings and/or arrays like itself
-        // Return the sum all the numbers you find, anywhere in the nest of arrays.
         var count = 0;
         for(var i=0, n=arr.length; i < n; i++) { 
             if (typeof arr[i] == "object") {
                 count += JS_BASICS.nestedSum(arr[i]);
             }
-            if (Number.isInteger(arr[i])) {  
+            if (typeof arr[i] === "number") {
                 count += arr[i]; 
             }
         }
